98ec0840611e156d1bf090b312f0a356eaa5d66d
bob1983 <bob.1983.g@gmail.com>
2019-07-06 00:02:11 +0900

    Dynamic import example

--- typescript-getstarted/src/import_type_inference.ts
+++ typescript-getstarted/src/import_type_inference.ts
@@ -5,3 +5,16 @@ import { value, label, returnFalse } from './exportee'
 const v1 = value
 const v2 = label
 const v3 = returnFalse()
+
+/**
+ * dynamic import
+ */
+
+ import('./exportee').then(module => {
+   const amount = module.value
+ })
+
+ async function sample() {
+   const { value } = await import('./exportee')
+   const amount = value
+ }
