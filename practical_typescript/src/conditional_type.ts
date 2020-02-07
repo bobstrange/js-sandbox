919ce1a43444a5500142568706286d58a11f1215
bob1983 <bob.1983.g@gmail.com>
2020-02-07 22:08:58 +0900

    conditional type promise.resolve

--- practical_typescript/src/conditional_type.ts
+++ practical_typescript/src/conditional_type.ts
@@ -9,3 +9,13 @@ type Args<T> = T extends (...arg: infer U) => any ? U : never
 type A = Arg1<typeof greet> // type A = string
 type B = Arg2<typeof greet> // type B = number
 type C = Args<typeof greet> // type C = [string, number]
+
+{
+  const foo = async () => {
+    return `Foo`
+  }
+
+  type ResolveArg<T> = T extends () => Promise<infer U> ? U : never
+  type X = typeof foo // type X = () => Promise<string>
+  type Y = ResolveArg<typeof foo> // type Y = string
+}
