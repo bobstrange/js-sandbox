e11f4cf81eae6c75a01dc782dbd9a066b9221c0f
bob1983 <bob.1983.g@gmail.com>
2020-02-10 23:55:42 +0900

    added marker

--- lecture/src/index.ts
+++ lecture/src/index.ts
@@ -1,5 +1,10 @@
-// import { User } from './User'
-// import { Company } from './Company'
+import { User } from './User'
+import { Company } from './Company'
 import { CustomMap } from './CustomMap'
 
 const map = new CustomMap('map')
+const user = new User()
+map.addUserMarker(user)
+
+const company = new Company()
+map.addCompanyMarker(company)
