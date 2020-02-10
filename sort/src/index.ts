57cb41862a0e16af13a85beb9c5f0e4f43c40212
bob1983 <bob.1983.g@gmail.com>
2020-02-11 01:44:33 +0900

    sorter impl

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,21 +1,14 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from "./Sorter"
+import { NumbersCollection } from "./NumbersCollection"
+import { CharactersCollection } from "./CharactersCollection"
 
-  sort(): void {
-    const { length } = this.collection
+const numbersCollection = new NumbersCollection([10, 3, 5, -1, 0])
+const charactersCollection = new CharactersCollection('XaAaBb')
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j]
-          this.collection[j] = this.collection[j + 1]
-          this.collection[j + 1] = leftHand
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0])
+const sorter = new Sorter(numbersCollection)
 sorter.sort()
 console.log(sorter.collection)
+
+const charactersSorter = new Sorter(charactersCollection)
+charactersSorter.sort()
+console.log(charactersSorter.collection)
