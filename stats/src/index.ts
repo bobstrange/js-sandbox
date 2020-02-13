77385f55471e73ddd9577f01852ae40f8b66469e
bob1983 <bob.1983.g@gmail.com>
2020-02-13 23:54:32 +0900

    base impl

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,22 @@
 import fs from 'fs'
+
+const matches = fs.readFileSync(
+    'input.csv',
+    {
+      encoding: 'utf-8'
+    }
+  )
+  .split("\n")
+  .map((row: string) => row.split(','))
+
+
+let manUnitedWins = 0
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins += 1
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins += 1
+  }
+}
+
+console.log(manUnitedWins)
