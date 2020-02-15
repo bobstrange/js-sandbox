7d46f9e4e68fec9f43501084c8a8e11b6d6b24c4
bob1983 <bob.1983.g@gmail.com>
2020-02-16 00:57:52 +0900

    tmp impl

--- typescript_course/web/src/index.ts
+++ typescript_course/web/src/index.ts
@@ -1,5 +1,21 @@
 import { User } from "./models/User";
 
 const user = new User({ name: 'John', age: 20 })
-console.log(user.get('name'))
-console.log(user.get('age'))
+user.set({ name: 'Jane', age: 21 })
+
+user.on('change', () => {
+  console.log('change #1')
+})
+
+user.on('change', () => {
+  console.log('change #2')
+})
+
+user.on('save', () => {
+  console.log('save #1')
+})
+
+user.trigger('change')
+user.trigger('save')
+
+console.log(user)
