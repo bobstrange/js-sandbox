258dea3688e0c63419fcab64f36639d3530f7227
bob1983 <bob.1983.g@gmail.com>
2020-02-16 21:51:02 +0900

    Attributes class

--- typescript_course/web/src/models/User.ts
+++ typescript_course/web/src/models/User.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosResponse } from 'axios'
 import { Eventing } from './Eventing'
 import { Sync } from './Sync'
+import { Attributes } from './Attributes'
 
 export interface UserProps {
   id?: number
@@ -13,15 +13,9 @@ const rootURL = 'http://localhost:3000/users'
 export class User {
   public events: Eventing = new Eventing()
   public sync: Sync<UserProps> = new Sync<UserProps>(rootURL)
+  public attributes: Attributes<UserProps>
 
-  constructor(private data: UserProps) {}
-
-
-  get(propName: string): string|number {
-    return this.data[propName]
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update)
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs)
   }
 }
