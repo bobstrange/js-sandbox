8338b819529cebb30106e6bf4dcfe198b167d281
bob1983 <bob.1983.g@gmail.com>
2020-02-16 21:10:13 +0900

    Extract sync

--- typescript_course/web/src/models/User.ts
+++ typescript_course/web/src/models/User.ts
@@ -1,13 +1,21 @@
 import axios, { AxiosResponse } from 'axios'
+import { Eventing } from './Eventing'
+import { Sync } from './Sync'
 
-interface UserProps {
+export interface UserProps {
   id?: number
   name?: string
   age?: number
 }
 
+const rootURL = 'http://localhost:3000/users'
+
 export class User {
+  public events: Eventing = new Eventing()
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootURL)
+
   constructor(private data: UserProps) {}
+
 
   get(propName: string): string|number {
     return this.data[propName]
@@ -15,22 +23,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update)
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data)
-      })
-  }
-
-  save(): void {
-    const id = this.get('id')
-
-    if (id)  {
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-    } else {
-      axios.post('http://localhost:3000/users', this.data)
-    }
   }
 }
