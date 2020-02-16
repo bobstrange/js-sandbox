95f4954206cb8db102dd5e958feb6882f84ed0a0
bob1983 <bob.1983.g@gmail.com>
2020-02-16 23:31:16 +0900

    composition based User

--- typescript_course/web/src/models/User.ts
+++ typescript_course/web/src/models/User.ts
@@ -30,4 +30,31 @@ export class User {
   get get() {
     return this.attributes.get
   }
+
+  set(update: UserProps): void {
+    this.attributes.set(update)
+    this.events.trigger('change')
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id')
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id')
+    }
+
+    this.sync.fetch(id).then((response): void => {
+      this.set(response.data)
+    })
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response): void => {
+        this.trigger('save')
+      })
+      .catch((error) => {
+        this.trigger('error')
+      })
+  }
 }
