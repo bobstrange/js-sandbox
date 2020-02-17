7970f1975cacdaa11eb6f5395098254032a9b2b1
bob1983 <bob.1983.g@gmail.com>
2020-02-17 17:24:47 +0900

    base model class

--- typescript_course/web/src/models/User.ts
+++ typescript_course/web/src/models/User.ts
@@ -1,6 +1,7 @@
-import { Eventing } from './Eventing'
-import { Sync } from './Sync'
-import { Attributes } from './Attributes'
+import { Model } from "./Model"
+import { Attributes } from  "./Attributes"
+import { ApiSync } from "./ApiSync"
+import { Eventing } from "./Eventing"
 
 export interface UserProps {
   id?: number
@@ -10,51 +11,12 @@ export interface UserProps {
 
 const rootURL = 'http://localhost:3000/users'
 
-export class User {
-  public events: Eventing = new Eventing()
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootURL)
-  public attributes: Attributes<UserProps>
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs)
-  }
-
-  get on() {
-    return this.events.on
-  }
-
-  get trigger() {
-    return this.events.trigger
-  }
-
-  get get() {
-    return this.attributes.get
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update)
-    this.events.trigger('change')
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id')
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id')
-    }
-
-    this.sync.fetch(id).then((response): void => {
-      this.set(response.data)
-    })
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((response): void => {
-        this.trigger('save')
-      })
-      .catch((error) => {
-        this.trigger('error')
-      })
+export class User extends Model<UserProps> {
+  static build(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootURL)
+    )
   }
 }
