9b1e844e72e126c9a6927521b69f5326cd2453ce
bob1983 <bob.1983.g@gmail.com>
2020-02-17 20:48:48 +0900

    nesting view

--- typescript_course/web/src/views/View.ts
+++ typescript_course/web/src/views/View.ts
@@ -1,6 +1,8 @@
 import { Model } from "../models/Model"
 
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } =  {}
+
   constructor(
     public parent: Element,
     public model: T
@@ -8,8 +10,15 @@ export abstract class View<T extends Model<K>, K> {
     this.bindModel()
   }
 
-  abstract eventsMap(): { [key: string]: () => void }
   abstract template(): string
+
+  regionsMap(): { [key: string]: string } {
+    return {}
+  }
+
+  eventsMap(): { [key: string]: () => void }  {
+    return {}
+  }
 
   bindModel() {
     this.model.on('change', () => {
@@ -28,12 +37,31 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
-  render(): void {
-    this.parent.innerHTML = ''
-    const templateElement = document.createElement('template')
-    templateElement.innerHTML = this.template()
-    this.bindEvents(templateElement.content)
-    this.parent.append(templateElement.content)
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap()
+    for (let key in regionsMap) {
+      const selector = regionsMap[key]
+      const element = fragment.querySelector(selector)
+      if (element) {
+        this.regions[key] = element
+      }
+    }
   }
 
+  onRender(): void {
+  }
+
+  render(): void {
+    this.parent.innerHTML = ''
+
+    const templateElement = document.createElement('template')
+    templateElement.innerHTML = this.template()
+
+    this.bindEvents(templateElement.content)
+    this.mapRegions(templateElement.content)
+
+    this.onRender()
+
+    this.parent.append(templateElement.content)
+  }
 }
