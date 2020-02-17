f2363e7c17360735e9e2e02fba50816e5047f0d9
bob1983 <bob.1983.g@gmail.com>
2020-02-17 22:44:37 +0900

    login

--- typescript_course/server/src/index.ts
+++ typescript_course/server/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
 import { router } from './routes/loginRoutes'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({ keys: ['dajfkdjak']}))
 app.use(router)
 
 app.listen(3000, () => {
