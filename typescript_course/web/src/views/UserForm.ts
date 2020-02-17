8a79200ad5d9431e85682456ff009ca1df04a6dd
bob1983 <bob.1983.g@gmail.com>
2020-02-17 19:56:43 +0900

    base view class

--- typescript_course/web/src/views/UserForm.ts
+++ typescript_course/web/src/views/UserForm.ts
@@ -1,19 +1,7 @@
 import { User } from "../models/User"
+import { View } from "./View"
 
-export class UserForm {
-  constructor(
-    public parent: Element,
-    public model: User
-  ) {
-    this.bindModel()
-  }
-
-  bindModel() {
-    this.model.on('change', () => {
-      this.render()
-    })
-  }
-
+export class UserForm extends View {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-name': this.onSetNameClick,
@@ -23,8 +11,10 @@ export class UserForm {
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input')
-    const newName = input.value
-    this.model.set({ name: newName })
+    if (input) {
+      const newName = input.value
+      this.model.set({ name: newName })
+    }
   }
 
   onSetAgeClick = (): void => {
@@ -42,24 +32,5 @@ export class UserForm {
         <button class="set-age">Set Random Age</button>
       </div>
     `
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap()
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':')
-      fragment.querySelectorAll(selector).forEach(element => {
-        element.addEventListener(eventName, eventsMap[eventKey])
-      })
-    }
-  }
-
-  render(): void {
-    this.parent.innerHTML = ''
-    const templateElement = document.createElement('template')
-    templateElement.innerHTML = this.template()
-    this.bindEvents(templateElement.content)
-    this.parent.append(templateElement.content)
   }
 }
