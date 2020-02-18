42bf50dfaf8a76f3bca1ddfeaa9c1ec355374678
bob1983 <bob.1983.g@gmail.com>
2020-02-18 22:58:28 +0900

    use enum

--- typescript_course/server/src/controllers/decorators/controller.ts
+++ typescript_course/server/src/controllers/decorators/controller.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata'
 import express from 'express'
+import { Methods } from './Methods'
+import { MetadataKeys } from './MetadataKeys'
 
 export const router = express.Router()
 
@@ -7,15 +9,16 @@ export function Controller(routePrefix: string) {
   return function(target: Function) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key]
-      const path = Reflect.getMetadata('path', target.prototype, key)
-      const method = Reflect.getMetadata('method', target.prototype, key)
+      const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key)
+      const method = Reflect.getMetadata(MetadataKeys.method, target.prototype, key) as  Methods
 
       console.log('Controller target: ', target, ' key: ', key, ' prototype: ', target.prototype)
 
       console.log('path: ', path)
       console.log('method: ', method)
+
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler)
+        router[method](`${routePrefix}${path}`, routeHandler)
       }
     }
   }
--- typescript_course/server/src/controllers/decorators/routes.ts
+++ typescript_course/server/src/controllers/decorators/routes.ts
@@ -1,18 +1,20 @@
 import 'reflect-metadata'
+import { Methods } from './Methods'
+import { MetadataKeys } from './MetadataKeys'
 
 function routeBinder(method: string) {
   return function (path: string) {
     return function(target: any, key: string, desc: PropertyDescriptor) {
       console.log('method:', method, ' path: ', path, ' target: ', target, ' key: ', key)
 
-      Reflect.defineMetadata('path', path, target, key)
-      Reflect.defineMetadata('method', method, target, key)
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key)
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key)
     }
   }
-
 }
 
-export const Get = routeBinder('get')
-export const Put = routeBinder('put')
-export const Post = routeBinder('post')
-export const Delete = routeBinder('delete')
+export const Get = routeBinder(Methods.get)
+export const Put = routeBinder(Methods.put)
+export const Post = routeBinder(Methods.post)
+export const Delete = routeBinder(Methods.delete)
+export const Patch = routeBinder(Methods.patch)
