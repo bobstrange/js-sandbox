a399d59f87d258efb79157baa9deb522906dda5c
bob1983 <bob.1983.g@gmail.com>
2020-02-20 00:25:07 +0900

    v-bind

--- vue-mastery/intro/main.js
+++ vue-mastery/intro/main.js
@@ -2,14 +2,9 @@ const app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks'
+    image: './assets/vmSocks-green-onWhite.jpg',
+    altText: 'A pair of socks',
+    link: 'https://google.co.jp'
   }
 })
 
-const app2 = new Vue({
-  el: '#app-two',
-  data: {
-    name: 'Jane',
-    age: 20
-  }
-})
