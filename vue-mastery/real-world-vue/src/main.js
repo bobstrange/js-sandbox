86ad5467db653ffcecc968ea816e546abeb6df42
bob1983 <bob.1983.g@gmail.com>
2020-02-20 21:33:37 +0900

    use global component

--- vue-mastery/real-world-vue/src/main.js
+++ vue-mastery/real-world-vue/src/main.js
@@ -1,12 +1,45 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+/** Automatic global registration */
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  '@/components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  // コンポーネント名をパスカルケース (PascalCase) で取得する
+  const componentName = upperFirst(
+    camelCase(
+      // フォルダの深さに関わらずファイル名を取得する
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+
+  Vue.component(
+    componentName,
+    // `export default` を使ってコンポーネントがエクスポートされた場合に存在する
+    // `.default` でコンポーネントオプションを期待していて
+    // 存在しない場合にはモジュールのルートにフォールバックします。
+    componentConfig.default || componentConfig
+  )
+})
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
