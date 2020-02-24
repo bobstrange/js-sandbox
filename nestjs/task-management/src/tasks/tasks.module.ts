5f6e5966d4b54c2d10fd6d07d4267cfeef2d5567
bob1983 <bob.1983.g@gmail.com>
2020-02-24 15:57:03 +0900

    AuthGuard for tasks

--- nestjs/task-management/src/tasks/tasks.module.ts
+++ nestjs/task-management/src/tasks/tasks.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TaskRepository } from './task.repository';
-import { Task } from './task.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TaskRepository])
+    TypeOrmModule.forFeature([TaskRepository]),
+    AuthModule
   ],
   controllers: [TasksController],
   providers: [TasksService]
