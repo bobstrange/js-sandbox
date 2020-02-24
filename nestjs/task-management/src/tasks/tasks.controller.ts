bda88868be0f499cefe99f3e795fac8473081db9
bob1983 <bob.1983.g@gmail.com>
2020-02-24 12:42:23 +0900

    update task status

--- nestjs/task-management/src/tasks/tasks.controller.ts
+++ nestjs/task-management/src/tasks/tasks.controller.ts
@@ -4,6 +4,7 @@ import { CreateTaskDTO } from './dto/create-task.dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
 
 @Controller('tasks')
 export class TasksController {
@@ -28,13 +29,13 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDTO)
   }
 
-  // @Patch('/:id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', new TaskStatusValidationPipe()) status: TaskStatus
-  // ): Task {
-  //   return this.tasksService.updateTaskStatus(id, status)
-  // }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', new TaskStatusValidationPipe()) status: TaskStatus
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status)
+  }
 
   @Delete('/:id')
   deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
