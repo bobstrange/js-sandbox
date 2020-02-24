5970fb5b8ed49832c8b2379bab70469afdc79952
bob1983 <bob.1983.g@gmail.com>
2020-02-24 14:38:17 +0900

    avoid duplicate usernames

--- nestjs/task-management/src/auth/user.repository.ts
+++ nestjs/task-management/src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from "typeorm"
 import { User } from "./user.entity"
 import { AuthCredentialsDTO } from "./dto/auth-credentials.dto"
+import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,6 +10,13 @@ export class UserRepository extends Repository<User> {
     const user = new User()
     user.username = username
     user.password = password
-    await user.save()
+    try {
+      await user.save()
+    } catch (error) {
+      if (error.code === '23505') { // duplicate username
+        throw new ConflictException('Username already exists')
+      }
+      throw new InternalServerErrorException()
+    }
   }
 }
