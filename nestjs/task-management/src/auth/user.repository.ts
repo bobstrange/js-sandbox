13f5857869afc2f00eb386802cef0139e5142410
bob1983 <bob.1983.g@gmail.com>
2020-02-24 14:51:52 +0900

    Added salt

--- nestjs/task-management/src/auth/user.repository.ts
+++ nestjs/task-management/src/auth/user.repository.ts
@@ -2,14 +2,18 @@ import { Repository, EntityRepository } from "typeorm"
 import { User } from "./user.entity"
 import { AuthCredentialsDTO } from "./dto/auth-credentials.dto"
 import { ConflictException, InternalServerErrorException } from "@nestjs/common"
+import * as bcrypt from 'bcrypt'
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
     const { username, password } = authCredentialsDTO
+
     const user = new User()
     user.username = username
-    user.password = password
+    user.salt =  await bcrypt.genSalt()
+    user.password = await this.hashPassword(password, user.salt)
+
     try {
       await user.save()
     } catch (error) {
@@ -18,5 +22,9 @@ export class UserRepository extends Repository<User> {
       }
       throw new InternalServerErrorException()
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt)
   }
 }
