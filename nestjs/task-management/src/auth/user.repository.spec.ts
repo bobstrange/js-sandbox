e713e2667dbda6d5060ed73cef954453ed01a72c
bob1983 <bob.1983.g@gmail.com>
2020-02-26 19:12:16 +0900

    validateUserPassword test

--- nestjs/task-management/src/auth/user.repository.spec.ts
+++ nestjs/task-management/src/auth/user.repository.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from "@nestjs/testing";
 import { UserRepository } from "./user.repository";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { User } from "./user.entity";
 
 const mockCreadentialsDTO = { username: 'test user', password: 'testpassword'}
 
@@ -34,8 +35,42 @@ describe('UserRepository', () => {
     })
 
     it('throws a InternalServerErrorException', () => {
-      save.mockRejectedValue({ code: '12345' })
+      save.mockRejectedValue({ code: '12345' }) // Unhandled error code
       expect(userRepository.signUp(mockCreadentialsDTO)).rejects.toThrow(InternalServerErrorException)
+    })
+  })
+
+  describe('validateUserPassword', () => {
+    let user
+
+    beforeEach(() => {
+      userRepository.findOne = jest.fn()
+      user = new User()
+      user.username = 'Test username'
+      user.validatePassword = jest.fn()
+    })
+
+    it('returns the username as validation is successful', async () => {
+      userRepository.findOne.mockResolvedValue(user)
+      user.validatePassword.mockResolvedValue(true)
+
+      const result = await userRepository.validateUserPassword(mockCreadentialsDTO)
+      expect(result).toEqual('Test username')
+    })
+
+    it('returns null as user cannot be found', async () => {
+      userRepository.findOne.mockResolvedValue(null)
+      const result = await userRepository.validateUserPassword(mockCreadentialsDTO)
+      expect(user.validatePassword).not.toHaveBeenCalled()
+      expect(result).toBeNull()
+    })
+
+    it('returns null as password is invalid', async () => {
+      userRepository.findOne.mockResolvedValue(user)
+      user.validatePassword.mockResolvedValue(false)
+      const result = await userRepository.validateUserPassword(mockCreadentialsDTO)
+      expect(user.validatePassword).toHaveBeenCalled()
+      expect(result).toBeNull()
     })
   })
 })
