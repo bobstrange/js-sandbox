c3fce6b361c69f74f9fd2a0e072a7cda475c3595
bob1983 <bob.1983.g@gmail.com>
2020-03-04 17:44:51 +0900

    wip

--- vue/video-browser/src/App.vue
+++ vue/video-browser/src/App.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>Hi there!</div>
+  <div>
+    <SearchBar @searchTermChange="onSearchTermChange"></SearchBar>
+  </div>
 </template>
 
 <script>
+import SearchBar from './components/SearchBar'
+import axios from 'axios'
+import { API_KEY } from '../tmp-secret'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    SearchBar
+  },
+  methods: {
+    async onSearchTermChange(searchTerm) {
+      console.log('apikey', API_KEY)
+      const result = await axios.get(
+        'https://www.google.apis.com/youtube/v3/search',
+        {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm
+          }
+        }
+      )
+      console.log(result.data)
+    }
+  }
 }
 </script>
 
---
+++ vue/video-browser/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" @input="onInput">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput(event) {
+      this.$emit('searchTermChange', event.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
