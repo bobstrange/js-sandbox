100370420be562ea541390d1718e918042028e4c
bob1983 <bob.1983.g@gmail.com>
2020-03-04 20:16:58 +0900

    imgur

---
+++ vue/images/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="#" class="item title">
+      <h1>Image Storage</h1>
+    </a>
+
+    <div class="right menu">
+      <a class="item">
+        Messages
+      </a>
+      <a class="item">
+        Friends
+      </a>
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1.5rem;
+}
+
+</style>
