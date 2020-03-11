08f2511ad96bb49326312fd7126e8d872a137ed7
bob1983 <bob.1983.g@gmail.com>
2020-03-11 18:34:18 +0900

    Custom validator

--- nestjs/tasks/src/tasks/tasks.controller.ts
+++ nestjs/tasks/src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -24,10 +25,10 @@ export class TasksController {
     return this.tasksService.getTaskById(id)
   }
 
-  @Patch('/:id')
+  @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus
+    @Body('status', new TaskStatusValidationPipe()) status: TaskStatus
   ): Task {
     return this.tasksService.updateTaskStatue(id, status)
   }
