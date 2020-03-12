e323d5cabaf30e82d1ea6ed76d69272eac3d0d42
bob1983 <bob.1983.g@gmail.com>
2020-03-12 13:18:33 +0900

    Before

--- typescript/typeorm-playground/src/index.ts
+++ typescript/typeorm-playground/src/index.ts
@@ -1,21 +1,18 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import express, { Request, Response } from "express"
 
-createConnection().then(async connection => {
+const app = express()
+app.use(express.json())
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+app.get('/users', (req: Request, res: Response) => {
+  console.log('/users')
+})
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+app.get('/users/:id', (req: Request, res: Response) => {
+  console.log('/users')
+})
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+app.post('/users', (req: Request, res: Response) => {
+  console.log('/users')
+})
 
-}).catch(error => console.log(error));
+app.listen(3000)
