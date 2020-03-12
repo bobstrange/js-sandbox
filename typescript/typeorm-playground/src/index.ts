b9f9500d4ebbd14fc756534e81f184e5b6bb7f11
bob1983 <bob.1983.g@gmail.com>
2020-03-12 13:03:12 +0900

    run typeorm init

--- typescript/typeorm-playground/src/index.ts
+++ typescript/typeorm-playground/src/index.ts
@@ -1,10 +1,21 @@
-import express from 'express'
-const app = express()
-const port = parseInt(process.env.PORT || '3000')
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
+createConnection().then(async connection => {
 
-app.listen(port, () => console.log(`Listening on port ${port}`) )
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log("Here you can setup and run express/koa/any other framework.");
+
+}).catch(error => console.log(error));
