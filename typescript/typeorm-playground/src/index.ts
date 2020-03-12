0e9f73bd0cfcb8546b61fdc32ebc8d41082b2b18
bob1983 <bob.1983.g@gmail.com>
2020-03-12 14:46:03 +0900

    ormconfig.json to ormconfig.js

--- typescript/typeorm-playground/src/index.ts
+++ typescript/typeorm-playground/src/index.ts
@@ -1,18 +1,38 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response, NextFunction } from "express"
+import { createConnection } from "typeorm"
 
-const app = express()
-app.use(express.json())
+import { User } from "./entity/User"
 
-app.get('/users', (req: Request, res: Response) => {
-  console.log('/users')
+createConnection().then(connection => {
+  const userRepository = connection.getRepository(User)
+
+  const app = express()
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+
+  app.get('/users', async (req: Request, res: Response) => {
+    const users = await userRepository.find()
+    res.json(users)
+  })
+
+  app.get('/users/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params
+    const user = await userRepository.findOne(id)
+
+    if (!user) {
+      const err = new Error(`User id: ${id} not found`)
+      return next(err)
+    }
+    return res.json(user)
+  })
+
+  app.post('/users', (req: Request, res: Response) => {
+    console.log('/users')
+  })
+
+  app.use((err, req, res, next) => {
+    res.status = err.statusCode || 500
+    res.send({ error: err })
+  })
+  app.listen(3000)
 })
-
-app.get('/users/:id', (req: Request, res: Response) => {
-  console.log('/users')
-})
-
-app.post('/users', (req: Request, res: Response) => {
-  console.log('/users')
-})
-
-app.listen(3000)
