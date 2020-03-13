d807db8fc02bde132ea47d98c923168412703504
bob1983 <bob.1983.g@gmail.com>
2020-03-13 23:46:03 +0900

    Define user schema

--- graphql/express-graphql/src/index.ts
+++ graphql/express-graphql/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express'
+import graphqlHTTP from "express-graphql";
+import schema from "./schema"
 
 const app = express()
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema
+}))
 
-app.get('/', (req: Request, res: Response) => { res.send('Hi there') })
 app.listen(8080, () => console.log('Listening on port 8080.'))
---
+++ graphql/express-graphql/src/schema.ts
@@ -0,0 +1,41 @@
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
+} from "graphql";
+
+
+const users = [
+  { id: '23', firstName: 'John', age: 20 },
+  { id: '43', firstName: 'Jane', age: 30 }
+]
+
+const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: userType,
+      args: { id: { type: GraphQLString }},
+      resolve(parentValue, args) {
+        return users.find(user => {
+          user.id === args.id
+        })
+      }
+    }
+  }
+})
+
+export default new GraphQLSchema({
+  query: RootQuery
+})
