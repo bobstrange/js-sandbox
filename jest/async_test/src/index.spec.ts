ad5a38b0d58b099df79f2a3b916a52d5aa7fb534
bob1983 <bob.1983.g@gmail.com>
2020-03-31 00:02:51 +0900

    resolve cases

--- jest/async_test/src/index.spec.ts
+++ jest/async_test/src/index.spec.ts
@@ -1,5 +1,17 @@
 import { fetchData, fetchDataReject } from './index'
 
-test('test fetch data', () => {
+// Resolve cases
+test('test fetch data (use resolve)', () => {
+  // アサーション(Promise)をreturnする必要がある
   return expect(fetchData()).resolves.toEqual('test')
+})
+
+test('test fetch data (use await)', async () => {
+  // 普通にawaitで待っても良い
+  expect(await fetchData()).toEqual('test')
+})
+
+test('test fetch data (use both)', async () => {
+  // アサーションを返す代わりに、awaitで待っても良い
+  await expect(fetchData()).resolves.toEqual('test')
 })
