eda442fb89fed9754b836558f57f44747c5337c4
bob1983 <bob.1983.g@gmail.com>
2020-03-31 13:17:39 +0900

    Added reject tests

--- jest/async_test/src/index.spec.ts
+++ jest/async_test/src/index.spec.ts
@@ -1,7 +1,8 @@
 import { fetchData, fetchDataReject } from './index'
 
 // Resolve cases
-test('test fetch data (use resolve)', () => {
+test('test fetch data (use resolves)', () => {
+  // 非同期の処理が完了するのを待ち受けるため
   // アサーション(Promise)をreturnする必要がある
   return expect(fetchData()).resolves.toEqual('test')
 })
@@ -15,3 +16,27 @@ test('test fetch data (use both)', async () => {
   // アサーションを返す代わりに、awaitで待っても良い
   await expect(fetchData()).resolves.toEqual('test')
 })
+
+// Reject cases
+test('test fetch data reject (use rejects)', () => {
+  // 非同期の処理が完了するのを待ち受けるため
+  // こちらもアサーション(Promise)をreturnする必要がある
+  return expect(fetchDataReject()).rejects.toEqual(new Error('error'))
+})
+
+test('test fetch data reject (use await)', async () => {
+  // 自前でawaitで待つときには、catchのアサーションが呼ばれていることを確認するために、想定するアサーションの数を設定しておく
+  // これがないと、Promiseがrejectされなかった場合(catch内が呼ばれなかった)にテストが失敗にならない。
+  expect.assertions(1)
+  try {
+    await fetchDataReject()
+  } catch (e) {
+    expect(e).toEqual(new Error('error'))
+  }
+})
+
+test('test fetch data reject (use await)', async () => {
+  // こちらもアサーションを返す代わりにawaitで待っても良い
+  await expect(fetchDataReject()).rejects.toThrow(new Error('error'))
+})
+
