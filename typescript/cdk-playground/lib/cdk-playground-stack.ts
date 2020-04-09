95939bc47a9721c77007976db34ad12134c9f284
bob1983 <bob.1983.g@gmail.com>
2020-04-09 19:24:09 +0900

    sample stack

--- typescript/cdk-playground/lib/cdk-playground-stack.ts
+++ typescript/cdk-playground/lib/cdk-playground-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3'
 
 export class CdkPlaygroundStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'bob-aws-cdk-test-bucket', {
+      versioned: true,
+      encryption: s3.BucketEncryption.KMS_MANAGED
+    })
   }
 }
