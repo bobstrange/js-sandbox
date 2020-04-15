972d0b30720bfdbdb84701fc1187b323c6e1efa5
bob1983 <bob.1983.g@gmail.com>
2020-04-15 23:16:16 +0900

    added server

--- graphql/app/server/server.js
+++ graphql/app/server/server.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
   type Query {
@@ -6,3 +6,17 @@ const typeDefs = gql`
   }
 `
 
+const resolvers = {
+  // TypeDefsの構造と一致している必要がある
+  Query: {
+    greeting: () => {
+      return 'Hello'
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen({ port: 8080 }).then((server) => {
+  console.log(`Server running at ${server.url}`)
+})
