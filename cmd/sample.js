a627976393c01dd52d2f43b86f75703f22221031
bob1983 <bob.1983.g@gmail.com>
2020-04-20 21:32:20 +0900

    Use pipeline

--- cmd/sample.js
+++ cmd/sample.js
@@ -1,1 +1,18 @@
-let test = 'Test -> var'
+const test = 'Test -> var'
+
+const func = async (arg) => {
+  console.log(arg)
+}
+
+const testMap = new Map()
+
+func('Hi')
+
+const double = (n) => n * 2
+const increment = (n) => n + 1
+
+const result = 10
+  |> double
+  |> increment
+  |> double
+console.log(result)
