14af1f1f2cc7eff8f1419f9a8ba2c2e4173feeef
bob1983 <bob.1983.g@gmail.com>
2020-04-28 20:04:00 +0900

    Added github integration tmp

--- react/finder/src/App.tsx
+++ react/finder/src/App.tsx
@@ -2,16 +2,44 @@ import React, { Component, Fragment } from "react"
 import "./App.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 
+import axios from 'axios'
 import { Navbar } from './components/Navbar'
 import { Users } from './components/Users'
+import { User } from './types/User'
+
+type AppState = {
+  users: User[],
+  loading: boolean
+}
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+    try {
+      const response = await axios.get('https://api.github.com/users')
+      this.setState({ users: response.data })
+    } catch (error) {
+      console.log(error)
+    } finally {
+      this.setState({ loading: false })
+    }
+  }
+
   render() {
+    const { loading, users } = this.state
     const content =
       <Fragment>
         <Navbar />
         <div className="container">
-          <Users />
+          <Users
+            loading={loading}
+            users={users}
+          />
         </div>
       </Fragment>
 
--- react/finder/src/components/Users.tsx
+++ react/finder/src/components/Users.tsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, FC } from 'react'
 import { UserItem } from './UserItem'
 import { User } from '../types/User'
 
-type UsersState = {
+type UsersProps = {
+  loading: boolean,
   users: User[]
 }
 
@@ -12,44 +13,19 @@ const userStyle = {
   gridGap: '1rem'
 }
 
-export class Users extends Component<{}, UsersState> {
-  state = {
-    users: [
-      {
-        id: "id",
-        login: "bob1983",
-        avatar_url: "https://avatars3.githubusercontent.com/u/1381585?s=460&v=4",
-        html_url: "https://github.com/bob1983"
-      },
-      {
-        id: "id",
-        login: "bob1983",
-        avatar_url: "https://avatars3.githubusercontent.com/u/1381585?s=460&v=4",
-        html_url: "https://github.com/bob1983"
-      },
-      {
-        id: "id",
-        login: "bob1983",
-        avatar_url: "https://avatars3.githubusercontent.com/u/1381585?s=460&v=4",
-        html_url: "https://github.com/bob1983"
-      }
-    ]
-  }
-
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.state.users.map(user => {
-          return (
-            <UserItem
-              key={user.id}
-              user={user}
-            />
-          )
-        })}
-      </div>
-    )
-  }
+export const Users: FC<UsersProps> = (props) => {
+  return (
+    <div style={userStyle}>
+      {props.users.map(user => {
+        return (
+          <UserItem
+            key={user.id}
+            user={user}
+          />
+        )
+      })}
+    </div>
+  )
 }
 
 export default Users
