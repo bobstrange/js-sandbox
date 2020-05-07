391b5740fc72aef08cb573caf9efb0591b0057ee
bob1983 <bob.1983.g@gmail.com>
2020-05-07 23:16:15 +0900

    make it stateful component

--- react/example/src/component/Tasks.tsx
+++ react/example/src/component/Tasks.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, Component } from 'react'
 
 interface Task {
   title: string
@@ -6,15 +6,58 @@ interface Task {
 interface TasksProps {
   tasks: Task[]
 }
+interface TasksState {
+  tasks: Task[]
+}
 
-export const Tasks: FC<TasksProps> = ({ tasks }) => {
-  return (
-    <ul>
-      {
-        tasks.map((task, i) => {
-          return <li key={i}>{task.title}</li>
-        })
-      }
-    </ul>
-  )
+// export const Tasks: FC<TasksProps> = ({ tasks }) => {
+//   return (
+//     <ul>
+//       {
+//         tasks.map((task, i) => {
+//           return <li key={i}>{task.title}</li>
+//         })
+//       }
+//     </ul>
+//   )
+// }
+
+export class Tasks extends Component<
+  TasksProps,
+  TasksState
+> {
+
+  constructor(props: TasksProps) {
+    super(props)
+    this.state = {
+      tasks: props.tasks
+    }
+  }
+
+  addNewTask = () => {
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        {
+          title: 'New task'
+        }
+      ]
+    })
+  }
+
+  render() {
+    const { tasks } = this.state
+    return (
+      <div className='TaskList'>
+        <ul>
+          {
+            tasks.map((task, i) => {
+              return <li key={i}>{task.title}</li>
+            })
+          }
+        </ul>
+        <button onClick={this.addNewTask}>Add new task</button>
+      </div>
+    )
+  }
 }
