dbc6e5638642ecfc84bd796556ff3e3ec8f09e25
bob1983 <bob.1983.g@gmail.com>
2020-05-16 22:17:20 +0900

    Not found impl

--- react/getting-started/src/route/Routes.tsx
+++ react/getting-started/src/route/Routes.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from '../layouts/Header'
 import AdminPage from '../admin/AdminPage'
 import ProductsPage from '../products/ProductsPage'
 import ProductPage from '../products/ProductPage'
+import NotFoundPage from '../NotFoundPage'
 
 const Routes: FC = () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <Route exact path="/products" component={ProductsPage} />
-        <Route exact path="/products/:id" component={ProductPage} />
-        <Route exact path="/admin" component={AdminPage} />
+        <Switch>
+          <Route exact path="/products" component={ProductsPage} />
+          <Route exact path="/products/:id" component={ProductPage} />
+          <Route exact path="/admin" component={AdminPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     </BrowserRouter>
   )
