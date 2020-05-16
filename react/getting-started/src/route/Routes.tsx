a78bf75be3781ef7a70c8feae01c2806b6596686
bob1983 <bob.1983.g@gmail.com>
2020-05-16 22:44:53 +0900

    Conditional routing

--- react/getting-started/src/route/Routes.tsx
+++ react/getting-started/src/route/Routes.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Header from '../layouts/Header'
 import AdminPage from '../admin/AdminPage'
@@ -8,6 +8,8 @@ import NotFoundPage from '../common/NotFoundPage'
 import LoginPage from '../common/LoginPage'
 
 const Routes: FC = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+
   return (
     <BrowserRouter>
       <div>
@@ -16,7 +18,9 @@ const Routes: FC = () => {
           <Redirect exact from="/" to="/products" />
           <Route exact path="/products" component={ProductsPage} />
           <Route exact path="/products/:id" component={ProductPage} />
-          <Route exact path="/admin" component={AdminPage} />
+          <Route exact path="/admin">
+            {loggedIn ? <AdminPage /> : <Redirect to="/login" />}
+          </Route>
           <Route exact path="/login" component={LoginPage} />
           <Route component={NotFoundPage} />
         </Switch>
