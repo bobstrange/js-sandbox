f660000156cf9af30611a4f09228b92d536b0af8
bob1983 <bob.1983.g@gmail.com>
2020-05-17 21:58:34 +0900

    search

--- react/getting-started/src/products/ProductsPage.tsx
+++ react/getting-started/src/products/ProductsPage.tsx
@@ -1,24 +1,37 @@
 import React, { FC, useState, useEffect } from 'react'
 import { Product, dummyProducts } from './Product'
 import './ProductsPage.css'
-import { Link } from 'react-router-dom'
+import { Link, RouteComponentProps } from 'react-router-dom'
+import 'url-search-params-polyfill'
 
-const ProductsPage: FC<{}> = () => {
+const ProductsPage: FC<RouteComponentProps<{}>> = (props) => {
   const [products, setProducts] = useState<Product[]>([])
+
+  const searchParams = new URLSearchParams(props.location.search)
+  const search = searchParams.get('search')
 
   useEffect(() => {
     setProducts(dummyProducts)
   }, [])
 
+  const productsToRender = search
+    ? products.filter(
+        (product) =>
+          product.name.toLowerCase().indexOf(search.toLowerCase()) > -1
+      )
+    : products
+
   return (
     <div className="page-container">
       <h1>TypeScriptBooks</h1>
       <ul className="product-list">
-        {products.map((product) => (
-          <li key={product.id} className="product-list__item">
-            <Link to={`/products/${product.id}`}>{product.name}</Link>
-          </li>
-        ))}
+        {productsToRender.length
+          ? productsToRender.map((product) => (
+              <li key={product.id} className="product-list__item">
+                <Link to={`/products/${product.id}`}>{product.name}</Link>
+              </li>
+            ))
+          : null}
       </ul>
     </div>
   )
