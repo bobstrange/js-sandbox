3ac04be9d3e7247d3260b8e83e6c4f4f76f17954
bob1983 <bob.1983.g@gmail.com>
2020-05-17 23:55:38 +0900

    Added record example

---
+++ typescript/basics/src/utility_types.ts
@@ -0,0 +1,22 @@
+// Partial
+
+interface Starship {
+  name: string
+  enableHyperjump: boolean
+}
+
+const updateStarship = (id: number, starship: Partial<Starship>) => {}
+updateStarship(1, { name: 'Explorer' })
+
+// Record
+
+const starships: Record<string, Starship> = {
+  Explorer1: {
+    name: 'Explorer1',
+    enableHyperjump: true
+  },
+  Explorer2: {
+    name: 'Explorer2',
+    enableHyperjump: false
+  }
+}
--- typescript/basics/src/utility_types/partial.ts
+++
@@ -1,7 +0,0 @@
-interface Starship {
-  name: string
-  enableHyperjump: boolean
-}
-
-const updateStarship = (id: number, starship: Partial<Starship>) => {}
-updateStarship(1, { name: 'Explorer' })
