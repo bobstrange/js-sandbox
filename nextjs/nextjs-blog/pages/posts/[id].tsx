47a3e3412edf4f53eec2e08f0999b7ff8ded87f8
bob1983 <bob.1983.g@gmail.com>
2020-07-18 23:13:18 +0900

    Added date component

--- nextjs/nextjs-blog/pages/posts/[id].tsx
+++ nextjs/nextjs-blog/pages/posts/[id].tsx
@@ -1,5 +1,7 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
 import { getPostIds, getPost, Post } from "../../lib/posts";
+import Date from "../../components/date";
 
 type getPostType = ReturnType<typeof getPost> extends Promise<infer T>
   ? T
@@ -8,11 +10,14 @@ type getPostType = ReturnType<typeof getPost> extends Promise<infer T>
 export default function PostPage({ postData }: { postData: getPostType }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
