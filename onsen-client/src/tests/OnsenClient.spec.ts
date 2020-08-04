9b428dba1c50ec0a9ae15be2ab14e909431f615f
bob1983 <bob.1983.g@gmail.com>
2020-08-04 22:38:19 +0900

    Added test

--- onsen-client/src/tests/OnsenClient.spec.ts
+++ onsen-client/src/tests/OnsenClient.spec.ts
@@ -4,8 +4,18 @@ describe('OnsenClient', () => {
   test('fetchPrograms', async () => {
     const client = new OnsenClient()
     const programs = await client.fetchPrograms()
-    console.log(programs[0])
-    expect(programs[0].id).toEqual(94)
+    const targetDirectoryName = 'toshitai'
+    const program = programs.find((program) => program.directory_name === targetDirectoryName)
+
+    if (program) {
+      const { id, directory_name, display, title, image, list, delivery_interval, delivery_day_of_week, category_list, pay, performers, related_links, related_programs, contents } = program
+      expect(id).toEqual(17)
+      expect(directory_name).toEqual('toshitai')
+      expect(title).toEqual('セブン-イレブン presents 佐倉としたい大西')
+      expect(display).toBeTruthy()
+    } else {
+      throw `Test is broken ${targetDirectoryName} should exist`
+    }
   })
 })
 
