f7c6f5b6fc124fb6a8a7125e8bcbffcc3c62d331
bobstrange <bob.1983.g@gmail.com>
2020-11-03 00:28:09 +0900

    added welcome dialog

---
+++ react/react-essential/pages/children.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+import { WelcomeDialog } from "../components/WelcomeDialog"
+
+const ChildrenPage = () => {
+  return (
+    <Layout title="Use children props">
+      <WelcomeDialog />
+    </Layout>
+  )
+}
+
+export default ChildrenPage
--- react/react-essential/pages/index.tsx
+++ react/react-essential/pages/index.tsx
@@ -1,30 +1,28 @@
 import Link from "next/link"
 import Layout from "../components/Layout"
 
+const PageLink: React.FC<{ linkTo: string; text: string }> = ({
+  linkTo,
+  text,
+}) => {
+  return (
+    <li>
+      <Link href={linkTo}>
+        <a>{text}</a>
+      </Link>
+    </li>
+  )
+}
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <ul style={{ listStyle: "none" }}>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/fc">
-          <a>FC</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/form">
-          <a>Form</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/state">
-          <a>State</a>
-        </Link>
-      </li>
+      <PageLink linkTo="/about" text="About" />
+      <PageLink linkTo="/fc" text="FC" />
+      <PageLink linkTo="/form" text="Form" />
+      <PageLink linkTo="/state" text="State" />
+      <PageLink linkTo="/children" text="Children" />
     </ul>
   </Layout>
 )
