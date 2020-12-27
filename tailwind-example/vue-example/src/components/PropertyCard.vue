a519c7fa32cfa6344d283883a14a4a1d84ea83a9
bobstrange <bob.1983.g@gmail.com>
2020-12-28 00:29:47 +0900

    tmp

--- tailwind-example/vue-example/src/components/PropertyCard.vue
+++ tailwind-example/vue-example/src/components/PropertyCard.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <img :src="property.image" :alt="property.image" />
-    <h4>{{ property.text }}</h4>
-    <ul>
-      <li v-for="(tag, i) in property.tags" :key="i">
-        {{ tag }}
-      </li>
-    </ul>
-    <div>
-      {{ property.formattedPrice }}
-    </div>
-    <div>
-      {{ property.rating }}/5 stars (based on {{ property.reviewCount }} reviews
-      and {{ property.likes }} likes)
+  <div class="bg-white border rounded-lg overflow-hidden max-w-sm">
+    <img class="max-h-32" :src="property.image" :alt="property.image" />
+    <div class="p-6">
+      <h4 class="font-semibold text-lg text-gray leading-tight truncate">
+        {{ property.text }}
+      </h4>
+      <ul class="text-gray-600 text-xs uppercase tracking-wider mt-1">
+        <li v-for="(tag, i) in property.tags" :key="i">
+          {{ tag }}
+        </li>
+      </ul>
+      <div>
+        {{ property.formattedPrice }}
+        <span class="text-gray-600 text-sm">/ wk</span>
+      </div>
+      <div class="mt-4">
+        <span class="text-indigo-600 font-semibold">
+          {{ property.rating }}/5 stars
+        </span>
+        <span class="text-gray-600 text-sm">
+          (based on {{ property.reviewCount }} reviews and
+          {{ property.likes }} likes)</span
+        >
+      </div>
     </div>
   </div>
 </template>
