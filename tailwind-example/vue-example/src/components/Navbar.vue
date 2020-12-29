750eab4b8f0c11a720c8cc92ad8e352662fb2139
bobstrange <bob.1983.g@gmail.com>
2020-12-30 00:04:21 +0900

    feat: make navigation bar responsive

--- tailwind-example/vue-example/src/components/Navbar.vue
+++ tailwind-example/vue-example/src/components/Navbar.vue
@@ -1,11 +1,13 @@
 <template>
-  <header class="bg-gray-400">
-    <div class="flex items-center justify-between px-4 py-1">
+  <header
+    class="bg-gray-400 sm:flex sm:items-center sm:justify-between sm:px-4 sm:py-1"
+  >
+    <div class="flex items-center justify-between px-4 py-1 sm:p-0">
       <div class="h-10 overflow-hidden">
         <img class="h-full" src="/discord.png" alt="discord" />
       </div>
-      <div>
-        <button type="button block" class="">
+      <div class="sm:hidden">
+        <button type="button block" @click="toggle" class="">
           <img
             class="h-6 w-6 fill-current"
             src="/humberger.png"
@@ -14,20 +16,20 @@
         </button>
       </div>
     </div>
-    <div :class="humbergerClass" class="px-2 pt-2 pb-4">
+    <div :class="humbergerClass" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
       <a
         href="#"
-        class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-300"
+        class="block px-2 py-1 text-sm text-white font-semibold rounded hover:bg-gray-300"
         >Item 1</a
       >
       <a
         href="#"
-        class="block px-2 py-1 mt-1 text-white font-semibold rounded hover:bg-gray-300"
+        class="block px-2 py-1 mt-1 text-sm text-white font-semibold rounded hover:bg-gray-300 sm:mt-0 sm:ml-2"
         >Item 2</a
       >
       <a
         href="#"
-        class="block px-2 py-1 mt-1 text-white font-semibold rounded hover:bg-gray-300"
+        class="block px-2 py-1 mt-1 text-sm text-white font-semibold rounded hover:bg-gray-300 sm:mt-0 sm:ml-2"
         >Item 3</a
       >
     </div>
@@ -43,8 +45,10 @@ export default defineComponent({
     const humbergerClass = computed(() => {
       return isOpen.value ? 'block' : 'hidden'
     })
-    console.log(humbergerClass)
-    return { isOpen, humbergerClass }
+    const toggle = () => {
+      isOpen.value = !isOpen.value
+    }
+    return { isOpen, humbergerClass, toggle }
   }
 })
 </script>
