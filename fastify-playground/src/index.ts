68828813fdead76f903f7d6998c8a0ae2cd1cdbd
bobstrange <bob.1983.g@gmail.com>
2021-02-28 00:12:02 +0900

    fix: use async/await to start server

--- fastify-playground/src/index.ts
+++ fastify-playground/src/index.ts
@@ -1,13 +1,14 @@
-import fastify from 'fastify'
-
 import { server } from '~/server'
 import { PORT } from '~/config/environments'
 
-server.listen(PORT, (err, address) => {
-  if (err) {
-    console.error(err)
+const start = async () => {
+  try {
+    await server.listen(PORT)
+  } catch (error) {
+    server.log.error(error)
     process.exit(1)
   }
-
   console.log(`server listening at ${PORT}`)
-})
+}
+
+start()
