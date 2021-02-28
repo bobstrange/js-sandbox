0ee4cd9eac42f4634a2057523c52ad717ecc6ea8
bobstrange <bob.1983.g@gmail.com>
2021-02-28 23:21:57 +0900

    feature: typed preValidation

--- fastify-playground/src/server.ts
+++ fastify-playground/src/server.ts
@@ -18,13 +18,23 @@ interface IHeaders {
 server.get<{
   Querystring: IQuerystring
   Headers: IHeaders
-}>('/example', async (request, reply) => {
-  const { name, status } = request.query
-  const customHeader = request.headers['h-Custom']
+}>(
+  '/example',
+  {
+    preValidation: (request, reply, done) => {
+      const { name, status } = request.query
+      done(name !== 'admin' ? new Error('name must be admin') : undefined)
+    },
+  },
+  async (request, reply) => {
+    const { name, status } = request.query
+    const customHeader = request.headers['h-Custom']
 
-  console.log(name, status)
-  console.log(customHeader)
+    console.log(name, status)
+    console.log(customHeader)
 
-  return 'Logged in !'
-})
+    return 'Logged in !'
+  }
+)
+
 export { server }
