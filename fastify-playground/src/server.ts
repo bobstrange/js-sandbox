483252980452eb0351a785859640adadbe0dfb6a
bobstrange <bob.1983.g@gmail.com>
2021-02-28 23:07:13 +0900

    feat: add example route

--- fastify-playground/src/server.ts
+++ fastify-playground/src/server.ts
@@ -6,4 +6,25 @@ server.get('/ping', async (req, res) => {
   return 'pong\n'
 })
 
+interface IQuerystring {
+  name: string
+  status: string
+}
+
+interface IHeaders {
+  'h-Custom': string
+}
+
+server.get<{
+  Querystring: IQuerystring
+  Headers: IHeaders
+}>('/example', async (request, reply) => {
+  const { name, status } = request.query
+  const customHeader = request.headers['h-Custom']
+
+  console.log(name, status)
+  console.log(customHeader)
+
+  return 'Logged in !'
+})
 export { server }
