32d209554ad493eeab1388f9e28b8bfa9be12048
bobstrange <bob.1983.g@gmail.com>
2021-03-14 23:58:26 +0900

    feat: add Layout component

---
+++ gatsby/blog-example/src/components/layout.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './layout.css'
+
+const Layout: React.FC = ({ children }) => {
+  return <div>{children}</div>
+}
+
+export default Layout
--- gatsby/blog-example/src/pages/index.tsx
+++ gatsby/blog-example/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const IndexPage: React.FC = () => {
   return (
-    <>
+    <Layout>
       <h1>Home</h1>
       <p>Hi there</p>
       <Link to="/about/">About</Link>
-    </>
+    </Layout>
   )
 }
 
