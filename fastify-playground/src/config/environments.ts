c7f9cf46f74898fb7c86ecffebfafff053fd5d59
bobstrange <bob.1983.g@gmail.com>
2021-03-14 23:17:24 +0900

    feat: install prettier and format

--- fastify-playground/src/config/environments.ts
+++ fastify-playground/src/config/environments.ts
@@ -2,13 +2,13 @@ import 'dotenv/config'
 
 export const PORT = Number(process.env.PORT || '8080')
 
-if (!process.env.DB_PORT) {
-  throw new Error('DB_PORT is not defined')
-}
+// if (!process.env.DB_PORT) {
+//   throw new Error('DB_PORT is not defined')
+// }
 
-if (!process.env.DB_HOST) {
-  throw new Error('DB_HOST is not defined')
-}
+// if (!process.env.DB_HOST) {
+//   throw new Error('DB_HOST is not defined')
+// }
 
-export const DB_HOST = process.env.DB_HOST
-export const DB_PORT = Number(process.env.DB_PORT)
+// export const DB_HOST = process.env.DB_HOST
+// export const DB_PORT = Number(process.env.DB_PORT)
