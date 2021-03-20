4bb789b794667d1defc71cfc70d20109cf60a30c
bobstrange <bob.1983.g@gmail.com>
2021-03-21 00:01:25 +0900

    feat: add useSiteMetadata hook

--- gatsby/blog-example/src/components/layout.tsx
+++ gatsby/blog-example/src/components/layout.tsx
@@ -3,14 +3,17 @@ import { Header } from './header'
 import Helmet from 'react-helmet'
 
 import './layout.scss'
+import '../hooks/useSiteMetadata'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
 const Layout: React.FC = ({ children }) => {
+  const { title, description } = useSiteMetadata()
   return (
     <>
       <Helmet>
         <html lang="ja" />
-        <title>Example blog</title>
-        <meta name="description" content="site-description" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
       </Helmet>
       <Header />
       <main>{children}</main>
---
+++ gatsby/blog-example/src/hooks/useSiteMetadata.tsx
@@ -0,0 +1,18 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+export const useSiteMetadata = (): { title: string; description: string } => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  )
+
+  return data.site.siteMetadata
+}
