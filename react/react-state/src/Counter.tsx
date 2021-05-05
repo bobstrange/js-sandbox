e3148f5468a0c2a7f29a47d0771e9e3dc407ed85
bobstrange <bob.1983.g@gmail.com>
2021-05-05 22:00:55 +0900

    feat: save state to local storage

--- react/react-state/src/Counter.tsx
+++ react/react-state/src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import styles from './Counter.module.scss'
 
@@ -7,8 +7,25 @@ type Props = {
   step: number
 }
 
+const getStateFromLocalStorage = (): number => {
+  const storage = localStorage.getItem('counterState')
+  console.log(storage)
+  if (storage) {
+    const state = JSON.parse(storage)
+    if (typeof state.count === 'number') {
+      return state.count
+    }
+  }
+  return 0
+}
+
+const storeStateInLocalStorage = (count: number) => {
+  localStorage.setItem('counterState', JSON.stringify({ count }))
+  console.log(localStorage)
+}
+
 export const Counter: FC<Props> = ({ max, step }) => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(getStateFromLocalStorage())
 
   const increment = () =>
     setCount((count) => {
@@ -19,6 +36,14 @@ export const Counter: FC<Props> = ({ max, step }) => {
     })
   const decrement = () => setCount((count) => count - step)
   const reset = () => setCount(0)
+
+  useEffect(() => {
+    document.title = `Counter: ${count}`
+  }, [count])
+
+  useEffect(() => {
+    storeStateInLocalStorage(count)
+  }, [count])
 
   return (
     <div className={styles.Counter}>
