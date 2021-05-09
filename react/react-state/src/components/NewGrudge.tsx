3711e1644de4573d2de22f8b5f437063bbdf02e3
bobstrange <bob.1983.g@gmail.com>
2021-05-10 00:42:28 +0900

    feat: add grudge

--- react/react-state/src/components/NewGrudge.tsx
+++ react/react-state/src/components/NewGrudge.tsx
@@ -1,13 +1,22 @@
-import React, { FC, useState } from 'react'
+import React, { FC, FormEventHandler, useState } from 'react'
 
 import styles from './NewGrudge.module.scss'
 
-export const NewGrudge: FC = () => {
+type Props = {
+  onSubmit: (arg: { person: string; reason: string }) => void
+}
+
+export const NewGrudge: FC<Props> = ({ onSubmit }) => {
   const [person, setPerson] = useState('')
   const [reason, setReason] = useState('')
 
+  const submitHandler: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault()
+    onSubmit({ person, reason })
+  }
+
   return (
-    <form className={styles.NewGrudge}>
+    <form className={styles.NewGrudge} onSubmit={submitHandler}>
       <input
         type="text"
         className="hoge"
--- react/react-state/src/pages/GrudgesPage.tsx
+++ react/react-state/src/pages/GrudgesPage.tsx
@@ -1,12 +1,29 @@
 import React, { FC, useEffect, useState } from 'react'
 import { Layout } from '../components/Layout'
 import { GrudgeList } from '../components/GrudgeList'
+import { v4 as uuid } from 'uuid'
 
 import { Grudge, fetchGrudges } from '../models/grudge'
 import { NewGrudge } from '../components/NewGrudge'
 
 export const GrudgesPage: FC = () => {
   const [grudges, setGrudges] = useState<Grudge[]>([])
+
+  const addGrudge = ({
+    person,
+    reason,
+  }: {
+    person: string
+    reason: string
+  }) => {
+    const grudge = {
+      person,
+      reason,
+      id: uuid(),
+      forgiven: false,
+    }
+    setGrudges([...grudges, grudge])
+  }
 
   useEffect(() => {
     ;(async () => {
@@ -15,7 +32,7 @@ export const GrudgesPage: FC = () => {
   }, [])
   return (
     <Layout>
-      <NewGrudge />
+      <NewGrudge onSubmit={addGrudge} />
       <GrudgeList grudges={grudges} />
     </Layout>
   )
