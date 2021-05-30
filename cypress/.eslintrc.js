8b6117affa83e5290ec40fe88b16c85ec38660f2
bobstrange <bob.1983.g@gmail.com>
2021-05-30 20:00:22 +0900

    fix: fix lint setting

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'cypress/globals': true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["cypress", "@typescript-eslint"],
+  plugins: ['cypress', '@typescript-eslint'],
   rules: {},
-};
+}
