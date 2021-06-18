a890988105aa47317155b57db8bfd9d1f95efd04
bobstrange <bob.1983.g@gmail.com>
2021-06-18 21:57:16 +0900

    small fix

--- node/simple-server/index.js
+++ node/simple-server/index.js
@@ -1,6 +1,13 @@
 const http = require('http')
+const request = require('request')
 
 http.createServer((req, res) => {
-  res.end('Hi there')
+  request(
+    'https://jsonplaceholder.typicode.com/posts',
+    (err, resp, body) => {
+      const posts = JSON.parse(body)
+      res.end(JSON.stringify(posts[0]))
+    }
+  )
 }).listen(8080)
 
