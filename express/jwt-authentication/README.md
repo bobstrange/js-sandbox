2b9201eb950f98975ce0b51ade173d0a03057887
bobstrange <bob.1983.g@gmail.com>
2021-09-12 23:08:33 +0900

    feat: add devAuth server

---
+++ express/jwt-authentication/README.md
@@ -0,0 +1,6 @@
+# express JWT
+
+ref
+
+- <https://www.youtube.com/watch?v=Ud5xKCYQTjM>
+- <https://www.youtube.com/watch?v=mbsmsi7l3r4>
---
+++ express/jwt-authentication/src/authServer.mjs
@@ -0,0 +1,53 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json({
+    data: users,
+  })
+})
+
+app.post('/signup', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    console.log(hashedPassword)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).json({ data: user })
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const name = req.body.name
+  const password = req.body.password
+
+  const user = users.find((user) => user.name === name)
+
+  if (user == null) {
+    return res.status(400).json({ message: 'Cannot find user' })
+  }
+
+  try {
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ data: 'not allowed' })
+    }
+    const accessToken = jwt.sign({ user: { name } }, process.env.JWT_SECRET)
+    res.json({ accessToken })
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+})
+
+app.listen(4000)
