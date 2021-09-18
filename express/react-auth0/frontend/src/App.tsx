4840085ffe1ef386e73becfe01e2228d530728ff
bobstrange <bob.1983.g@gmail.com>
2021-09-18 23:34:27 +0900

    feat: add routes

--- express/react-auth0/frontend/src/App.tsx
+++ express/react-auth0/frontend/src/App.tsx
@@ -2,16 +2,36 @@ import React from 'react'
 import { css } from '@emotion/react'
 
 import { NavBar } from './components/NavBar'
+import { Route, Switch } from 'react-router-dom'
 
 const base = css`
   max-width: 80em;
   margin: 0 auto;
 `
 
+const Home: React.FC = () => {
+  return <h1>Home</h1>
+}
+
+const Profile: React.FC = () => {
+  return <h1>Profile</h1>
+}
+
+const ExternalApi: React.FC = () => {
+  return <h1>External API</h1>
+}
+
 function App() {
   return (
     <div className="App" css={base}>
       <NavBar />
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/external-api" exact component={ExternalApi} />
+        </Switch>
+      </div>
     </div>
   )
 }
--- express/react-auth0/frontend/src/components/NavBar.tsx
+++ express/react-auth0/frontend/src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react'
 import React from 'react'
 
 import { MainNav } from '../components/MainNav'
+import { AuthNav } from '../components/AuthNav'
 
 const container = css`
   background-color: black;
@@ -11,25 +12,12 @@ const container = css`
   align-items: center;
 `
 
-const button = css`
-  display: inline-block;
-  margin-right: 1rem;
-  border-radius: 0.4rem;
-  font-weight: bold;
-  color: #333;
-  font-size: 1.2rem;
-  background-color: #eee;
-  padding: 0.2em 1em;
-`
-
 export const NavBar: React.FC = () => {
   return (
     <div>
       <nav css={container}>
         <MainNav />
-        <div>
-          <button css={button}>Login</button>
-        </div>
+        <AuthNav />
       </nav>
     </div>
   )
