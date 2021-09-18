0aff9c9aeca517f6dae2dbd7a1d8408cea820410
bobstrange <bob.1983.g@gmail.com>
2021-09-19 00:05:35 +0900

    feat: show user info

--- express/react-auth0/frontend/src/views/Profile.tsx
+++ express/react-auth0/frontend/src/views/Profile.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { css } from '@emotion/react'
+
+const profileImage = css``
 
 export const Profile: React.FC = () => {
-  return <h1>Profile</h1>
+  const { user } = useAuth0()
+
+  if (!user) {
+    return null
+  }
+
+  const { picture, name, email } = user
+  return (
+    <div>
+      <div>
+        <img src={picture} alt="Profile" />
+        <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
+      </div>
+      <div>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+      </div>
+    </div>
+  )
 }
