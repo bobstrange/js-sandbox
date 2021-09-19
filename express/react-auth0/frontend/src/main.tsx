51d86d99e67afc5e81c6dd390a0fc8482df2fcbf
bobstrange <bob.1983.g@gmail.com>
2021-09-19 23:07:58 +0900

    refactor: rename

--- express/react-auth0/frontend/src/main.tsx
+++ express/react-auth0/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { Auth0ProviderWithHistory } from './auth/auth0-provider-with-history'
+import { Auth0ProviderWithHistory } from './auth/Auth0ProviderWithHistory'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
