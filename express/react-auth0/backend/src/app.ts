939cd635de9f9a8d7b2a3faa132fe3404e5f6768
bobstrange <bob.1983.g@gmail.com>
2021-09-19 23:49:59 +0900

    feat: add config

--- express/react-auth0/backend/src/app.ts
+++ express/react-auth0/backend/src/app.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import { clientOrigins } from './config/env.dev'
 
 export const app = express()
 
+app.use(helmet())
 app.use(express.json())
+app.use(cors({ origin: clientOrigins }))
 
 app.get('/', (req, res: Response) => {
   res.json({
