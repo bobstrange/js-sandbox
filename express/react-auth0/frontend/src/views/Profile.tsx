704d60417befd895e4d737f596166432f6733b93
bobstrange <bob.1983.g@gmail.com>
2021-09-19 22:48:29 +0900

    feat: styling profile page

--- express/react-auth0/frontend/src/views/Profile.tsx
+++ express/react-auth0/frontend/src/views/Profile.tsx
@@ -2,7 +2,41 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { css } from '@emotion/react'
 
-const profileImage = css``
+const container = css`
+  padding: 2em;
+`
+
+const vioContainer = css`
+  display: flex;
+  align-items: center;
+`
+
+const profileImage = css`
+  display: inline-block;
+  width: 10em;
+  height: 10em;
+`
+
+const info = css`
+  margin-left: 2em;
+`
+
+const emailStyle = css`
+  color: #999;
+  font-weight: bold;
+`
+
+const codeContainer = css`
+  margin-top: 5em;
+`
+
+const preStyle = css`
+  background-color: black;
+`
+
+const codeStyle = css`
+  color: white;
+`
 
 export const Profile: React.FC = () => {
   const { user } = useAuth0()
@@ -13,16 +47,18 @@ export const Profile: React.FC = () => {
 
   const { picture, name, email } = user
   return (
-    <div>
-      <div>
-        <img src={picture} alt="Profile" />
-        <div>
+    <div css={container}>
+      <div css={vioContainer}>
+        <img src={picture} alt="Profile" css={profileImage} />
+        <div css={info}>
           <h2>{name}</h2>
-          <p>{email}</p>
+          <p css={emailStyle}>{email}</p>
         </div>
       </div>
-      <div>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
+      <div css={codeContainer}>
+        <pre css={preStyle}>
+          <code css={codeStyle}>{JSON.stringify(user, null, 2)}</code>
+        </pre>
       </div>
     </div>
   )
