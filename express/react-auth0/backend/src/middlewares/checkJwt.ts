743be98abcdf1894b6b354ad4355fdcc92715683
bobstrange <bob.1983.g@gmail.com>
2021-09-20 22:02:49 +0900

    feat: add checkJWT middleware

---
+++ express/react-auth0/backend/src/middlewares/checkJwt.ts
@@ -0,0 +1,15 @@
+import jwt from 'express-jwt'
+import JwksRsa from 'jwks-rsa'
+import { domain, audience } from '../config/env.dev'
+
+export const checkJwt = jwt({
+  secret: JwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${domain}/.well-known/jwks.json`,
+  }),
+  audience,
+  issuer: `https://${domain}`,
+  algorithms: ['RS256'],
+})
