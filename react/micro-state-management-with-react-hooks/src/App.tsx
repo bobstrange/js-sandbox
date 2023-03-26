12215f0efb23b8e1848c107286f6d1d1fafc1fd3
bobstrange <bob.1983.g@gmail.com>
2023-03-27 00:21:05 +0900

    chore: global state with context 1

--- react/micro-state-management-with-react-hooks/src/App.tsx
+++ react/micro-state-management-with-react-hooks/src/App.tsx
@@ -1,15 +1,17 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
+import './App.css'
 
 import { ColorExample } from './components/ColorExample'
 import { CounterExample } from './components/CounterExample'
+import { GlobalCounterContextExample } from './components/GlobalCounterContextExample'
 
 function App() {
   return (
-    <>
+    <div className="app">
       <h1>Context Examples</h1>
       <ColorExample />
       <CounterExample />
-    </>
+      <GlobalCounterContextExample />
+    </div>
   )
 }
 
