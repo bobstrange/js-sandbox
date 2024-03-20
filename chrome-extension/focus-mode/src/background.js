0a58c929496b55e7b72be57e6226271307b395c2
bobstrange <bob.1983.g@gmail.com>
2024-03-20 23:48:53 +0900

    chore: change keybinding

--- chrome-extension/focus-mode/src/background.js
+++ chrome-extension/focus-mode/src/background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(() => {
 const extensions = "https://developer.chrome.com/docs/extensions";
 const webstore = "https://developer.chrome.com/docs/webstore";
 
-chrome.action?.onClicked.addListener(async (tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     const nextState = prevState === "ON" ? "OFF" : "ON";
