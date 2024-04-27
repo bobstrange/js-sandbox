e174ecfb2750dc72a8fd20a7c3160e63cc0f6fc1
bobstrange <bob.1983.g@gmail.com>
2024-04-28 00:21:59 +0900

    feat: show welcome page when extension is installed

--- chrome-extension/wxt-playground/entrypoints/background.ts
+++ chrome-extension/wxt-playground/entrypoints/background.ts
@@ -1,3 +1,14 @@
+import { browser } from "wxt/browser";
+
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log("Hello background!", { id: browser.runtime.id });
+
+  browser.runtime.onInstalled.addListener(async ({ reason }) => {
+    if (reason !== "install") return;
+
+    await browser.tabs.create({
+      url: browser.runtime.getURL("/welcome.html"),
+      active: true,
+    });
+  });
 });
