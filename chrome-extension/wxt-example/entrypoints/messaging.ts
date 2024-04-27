5dfb994bdeff9f56e465fa7d3e9020c3d824a6c4
bobstrange <bob.1983.g@gmail.com>
2024-04-28 00:02:16 +0900

    chore: tmp

--- chrome-extension/wxt-example/entrypoints/messaging.ts
+++
@@ -1,9 +0,0 @@
-import { defineExtensionMessaging } from "@webext-core/messaging";
-import { Todo } from "./types";
-
-interface ProtocolMap {
-  getTodo(id: number): Todo;
-}
-
-export const { sendMessage, onMessage } =
-  defineExtensionMessaging<ProtocolMap>();
--- chrome-extension/wxt-example/wxt.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'wxt';
-import react from '@vitejs/plugin-react';
-
-// See https://wxt.dev/api/config.html
-export default defineConfig({
-  vite: () => ({
-    plugins: [react()],
-  }),
-});
